e7378a1e9e01aac67fea2f292d76ad31ff30a33c
kalyan09122003 <kalyangk777@gmail.com>
2026-02-09 15:11:08 +0530

    Update Footer: Replace Twitter icon with X logo

--- src/app/components/Footer.jsx
+++ src/app/components/Footer.jsx
@@ -1,6 +1,6 @@
 import Link from "next/link";
 import Image from "next/image";
-import { Linkedin, Instagram, Facebook, Twitter } from "lucide-react";
+import { Linkedin, Instagram, Facebook } from "lucide-react";
 import WhatsAppIcon from "@mui/icons-material/WhatsApp";
 
 const Footer = () => {
@@ -91,11 +91,20 @@ const Footer = () => {
               <Facebook size={24} />
             </div>
 
-            {/* Twitter - Disabled */}
+            {/* X (Twitter) - Disabled */}
             <div
               className="text-[#0BA57F] opacity-60 cursor-default relative group"
             >
-              <Twitter size={24} />
+              {/* X Logo SVG */}
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                viewBox="0 0 24 24"
+                width="24"
+                height="24"
+                fill="currentColor"
+              >
+                <path d="M18.901 1.153h3.68l-8.04 9.19L24 22.846h-7.406l-5.8-7.584-6.638 7.584H.474l8.6-9.83L0 1.154h7.594l5.243 6.932ZM17.61 20.644h2.039L6.486 3.24H4.298Z" />
+              </svg>
             </div>
           </div>
         </div>
